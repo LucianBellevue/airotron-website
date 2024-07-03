4b24ac57971c150ee53decf2fa83f6aa53a8f2ac
Diego Medina <romeomdna@outlook.com>
2024-07-03 17:40:11 -0400

    testimonial and footer sections added with layout edit

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -3,10 +3,16 @@ import { TestimonialCards } from './TestimonialCards'
 
 const Testimonials = () => {
   return (
-    <section>
-      <h1>Testimonials</h1>
-      <TestimonialCards />
+    <section id="testimonials" className='bg:white py-10'>
+      <div className="container mx-auto px-4">
+        <div className="text-center mb-1">
+          <h2 className="text-3xl md:text-7xl font-bold text-gray-90">Kind words from our <span className='text-red-90'>Clients</span></h2>
+        </div>
+        <TestimonialCards />
+      </div>
     </section>
+
+
   )
 }
 
--- components/ui/infinite-moving-cards.tsx
+++ components/ui/infinite-moving-cards.tsx
@@ -87,10 +87,10 @@ export const InfiniteMovingCards = ({
       >
         {items.map((item, idx) => (
           <li
-            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
+            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-gray-100 px-8 py-6 md:w-[450px]"
             style={{
               background:
-                "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
+                "linear-gradient(180deg, var(--gray-100), var(--gray-200)",
             }}
             key={item.name}
           >
@@ -99,15 +99,15 @@ export const InfiniteMovingCards = ({
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
-              <span className=" relative z-20 text-sm leading-[1.6] text-gray-100 font-normal">
+              <span className=" relative z-20 text-sm leading-[1.6] text-gray-90 font-normal">
                 {item.quote}
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <span className="flex flex-col gap-1">
-                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
+                  <span className=" text-sm leading-[1.6] text-red-90 font-semibold">
                     {item.name}
                   </span>
-                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
+                  <span className=" text-sm leading-[1.6] text-red-900 font-semibold">
                     {item.title}
                   </span>
                 </span>
