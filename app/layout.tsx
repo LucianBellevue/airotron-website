6d7c1e62607acdd2c4b10ee2971ae8baf6294677
Diego Medina <romeomdna@outlook.com>
2024-07-06 16:02:51 -0400

    added social image to layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,12 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Airotron HVAC Services | Trusted Heating, Ventilation, and Air Conditioning Experts for Over 40 Years",
   description: "Airotron has been providing professional HVAC services to the community for over 40 years. We specialize in the installation, repair, and maintenance of heating, ventilation, and air conditioning systems for both residential and commercial properties. Trust our experienced technicians for reliable, affordable, and expert HVAC solutions. Your comfort is our priority. Contact Airotron today!",
+
 };
 
 export default function RootLayout({
@@ -15,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:image" content="https://airotron.com/og-image.png" />
+      </Head>
       <body>
       <Navbar />
        <main className="relative overflow-hidden">
